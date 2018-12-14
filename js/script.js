82b7c4d57a80220e49ab2b030623fb8be472ed95
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-14 12:44:23 +0100

    Add function to take data for form

--- js/script.js
+++ js/script.js
@@ -3,12 +3,12 @@ function Button(width, height, background, text, border, borderRadius, fontColor
 	this.height = height || 50;
 	this.background = background || "grey";
 	this.text = text || "przycisk";
-	this.border = border || 3;
+	this.border = border || 1;
 	this.borderRadius = borderRadius || 3;
 	this.fontColor = fontColor || "black";
 	this.fontSize = fontSize || 12;
 	this.padding = padding || 5;
-	this.isCenterText = isCenterText || true;
+	this.isCenterText = isCenterText || "center";
 	this.isDisabled = isDisabled || false;
 }
 
@@ -16,8 +16,22 @@ Button.prototype = {
 	create: function() {
 		let container = document.getElementById("container");
 
+		takeDataforButtons();
+
 		this.element = document.createElement("button");
-		this.element.innerHTML = "button";
+
+		this.element.style.width = buttonWitdh;
+		this.element.style.height = buttonHeight;
+		this.element.style.backgroundColor = buttonBackgroundcolor;
+		this.element.innerHTML = buttonText;
+		this.element.style.border = buttonBorder + " solid black";
+		this.element.style.borderRadius = buttonBorderRadius;
+		this.element.style.color = buttonFontColor;
+		this.element.style.fontSize = buttonFontSize;
+		this.element.style.padding = buttonPadding;
+		this.element.style.textAlign = buttonCentered;
+		this.element.disabled = buttonDisabled;
+
 		container.appendChild(this.element);
 	}
 }
@@ -27,4 +41,26 @@ function createButton() {
 
 	let button = new Button();
 	button.create();
+}
+
+function  takeDataforButtons() {
+	buttonWitdh = document.getElementById("button-width").value + "px";
+	buttonHeight = document.getElementById("button-height").value + "px";
+	buttonBackgroundcolor = document.getElementById("button-color").value;
+	buttonText = document.getElementById("button-text").value;
+	buttonBorder = document.getElementById("button-border").value + "px";
+	buttonBorderRadius = document.getElementById("button-border-radius").value + "px";
+	buttonFontColor = document.getElementById("button-font-color").value;
+	buttonFontSize = document.getElementById("button-font-size").value + "px";
+	buttonPadding = document.getElementById("button-padding").value + "px";
+
+	buttonDisabled = false;
+	if(document.getElementById("button-disabled").checked) {
+		buttonDisabled = true;
+	}
+
+	buttonCentered = "center";
+	if(document.getElementById("button-left-center").checked) {
+		buttonCentered = "left";
+	}
 }
