fa1da55b99eb1cb845d66f2fc21e7ffba8ca1316
Dawid Kowalski <dawid.k.kowalski@gmail.com>
2018-12-14 11:58:22 +0100

    Add simply functions to create buttons

--- js/script.js
+++ js/script.js
@@ -1,13 +1,30 @@
 function Button(width, height, background, text, border, borderRadius, fontColor, fontSize, padding, isCenterText, isDisabled) {
 	this.width = width || 100;
 	this.height = height || 50;
-	this.background = background || 'grey';
-	this.text = text || 'przycisk';
+	this.background = background || "grey";
+	this.text = text || "przycisk";
 	this.border = border || 3;
 	this.borderRadius = borderRadius || 3;
-	this.fontColor = fontColor || 'black';
+	this.fontColor = fontColor || "black";
 	this.fontSize = fontSize || 12;
 	this.padding = padding || 5;
 	this.isCenterText = isCenterText || true;
 	this.isDisabled = isDisabled || false;
+}
+
+Button.prototype = {
+	create: function() {
+		let container = document.getElementById("container");
+
+		this.element = document.createElement("button");
+		this.element.innerHTML = "button";
+		container.appendChild(this.element);
+	}
+}
+
+function createButton() {
+	let container = document.getElementById("container");
+
+	let button = new Button();
+	button.create();
 }
